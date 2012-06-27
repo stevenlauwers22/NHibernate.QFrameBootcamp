c8cf4542ab1cabfee4a7b9010c2dac81d4f056b0
Steven Lauwers <stevenlauwers22@hotmail.com>
2012-06-27 20:21:31 +0200

    Include sream sounds

--- NHibernateCourse.Demo7/Infrastructure/DontHurtMe/DontHurtMeInterceptor.cs
+++ NHibernateCourse.Demo7/Infrastructure/DontHurtMe/DontHurtMeInterceptor.cs
@@ -11,16 +11,16 @@ namespace NHibernateCourse.Demo7.Infrastructure.DontHurtMe
 		public override SqlString OnPrepareStatement(SqlString sql)
 		{
             var i = _numberOfRequest += 1;
-			if (i >= 8)
+			if (i >= 5)
 			{
 				var screams = new[]
 				{
-					"http://www.shockwave-sound.com/sound-effects/scream-sounds/ahhh.wav",
-					"http://www.shockwave-sound.com/sound-effects/scream-sounds/cri-d-effroi-scream.wav",
-					"http://www.shockwave-sound.com/sound-effects/scream-sounds/ciglik3.wav",
-					"http://www.shockwave-sound.com/sound-effects/scream-sounds/2scream.wav",
-					"http://www.shockwave-sound.com/sound-effects/scream-sounds/scream13.wav",
-					"http://www.shockwave-sound.com/sound-effects/scream-sounds/scream4.wav"
+					"Infrastructure/DontHurtMe/Sounds/ahhh.wav",
+					"Infrastructure/DontHurtMe/Sounds/cri-d-effroi-scream.wav",
+					"Infrastructure/DontHurtMe/Sounds/ciglik3.wav",
+					"Infrastructure/DontHurtMe/Sounds/2scream.wav",
+					"Infrastructure/DontHurtMe/Sounds/scream13.wav",
+					"Infrastructure/DontHurtMe/Sounds/scream4.wav"
 				};
 
 				var soundPlayer = new SoundPlayer(screams[i % screams.Length]);
